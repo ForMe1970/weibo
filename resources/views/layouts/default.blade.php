5ce7fa76acc7bd9ccb007ef17245c92a96ebb0be
ForMe1970 <ForMe1970@163.com>
2021-04-04 22:21:11 +0800

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,8 +2,22 @@
 <html>
     <head>
         <title>@yield('title', 'Weibo App') - Laravel 新手入门教程</title>
+        <link rel="stylesheet" href="/css/app.css">
     </head>
     <body>
+        <nav class="navbar weibo-nav">
+            <div class="container">
+                <a href="#" class="navbar-brand navbar-logo">Weibo App</a>
+                <ul class="nav navbar-nav navbar-menu">
+                    <li>
+                        <a href="#" class="navbar-link">帮助</a>
+                    </li>
+                    <li>
+                        <a href="#" class="navbar-link">登录</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
         @yield('content')
     </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,12 @@
 @extends('layouts.default')
 @section('title', '首页')
 @section('content')
-    <h1>主页</h1>
+<div class="container">
+	<div class="jumbotron">
+		<h1>Hello, world!</h1>
+		<p>任岁月流转，看物是人非。</p>
+		<p>一切，从这里开始。</p>
+		<p><a class="btn btn-primary btn-lg" href="#" role="button">现在注册</a></p>
+	</div>
+</div>
 @stop
