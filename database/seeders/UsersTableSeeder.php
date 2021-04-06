0cd689d1e2d6577caf98a875d2da81bef153021f
ForMe1970 <ForMe1970@163.com>
2021-04-06 23:33:41 +0800

    管理员可删除用户

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -19,6 +19,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'ForMe1970';
         $user->email = 'ForMe1970@163.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
