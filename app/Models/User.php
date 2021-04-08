3d7751f48a201ee8170ee4a946e5b9593339d6d5
ForMe1970 <ForMe1970@163.com>
2021-04-08 20:23:06 +0800

    微博数据模型

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,6 +42,11 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    public function statuses()
+    {
+        return $this->hasMany(Status::class);
+    }
+
     public static function boot()
     {
         parent::boot();
